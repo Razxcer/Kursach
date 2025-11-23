dee4c27920d3f2f62edce12d65685f2fa1d445c4
Razxcer <arbuzik31.31@gmail.com>
2025-11-23 11:22:41 +0700

    Vau

---
+++ vue-project/src/components/Head.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="head">
+
+    </div>
+
+</template>
+
+<style scoped>
+
+.head{
+    width: 100%;
+    height: 60px;
+    background-color: red;
+}
+
+
+
+
+@media (min-width: 1024px) {
+}
+</style>
---
+++ vue-project/src/components/Test.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="head">
+asdasdasdas
+    </div>
+
+</template>
+
+<style scoped>
+
+
+
+
+
+
+@media (min-width: 1024px) {
+}
+</style>
